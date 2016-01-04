71639f3ecbb085a8325c41d4b5880f76a06587e4
Philipp Wagner <mail@philipp-wagner.com>
2016-01-04 23:11:57 +0100

    Vagrant: create swap file
    Some processes (notably composer update) require a lot of memory and
    fail otherwise. Create a swap file to work around that.

---
+++ vagrant/create_swapfile.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# size of swapfile in megabytes
+swapsize=4000
+
+# does the swap file already exist?
+grep -q "swapfile" /etc/fstab
+
+# if not then create it
+if [ $? -ne 0 ]; then
+  echo 'swapfile not found. Adding swapfile.'
+  fallocate -l ${swapsize}M /swapfile
+  chmod 600 /swapfile
+  mkswap /swapfile
+  swapon /swapfile
+  echo '/swapfile none swap defaults 0 0' >> /etc/fstab
+else
+  echo 'swapfile found. No changes made.'
+fi
+
