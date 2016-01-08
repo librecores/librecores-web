cf7bd14d91c321d65cb063284d35467464a13455
Philipp Wagner <mail@philipp-wagner.com>
2016-01-08 23:26:28 +0100

    Create working staging deployment
    - Initialize with fixture data
    - Don't require AWS credentials for deployment

--- deploy-aws.sh
+++ deploy-aws.sh
@@ -34,7 +34,31 @@ Available ACTIONs:
 
   staging
     Deploy current code to stage.librecores.org
+
+  staging-initdata
+    Initialize stage.librecores.org with the data fixtures from Git, overwriting
+    all existing data.
 EOF
+}
+
+# Check if all required AWS credentials are set in the environment
+function ensure_aws_creds
+{
+  test -f aws-secrets.include && . aws-secrets.include
+  if [ -z "$AWS_ACCESS_KEY_ID" ] || [ -z "$AWS_SECRET_ACCESS_KEY" ]; then
+    echo Error: No AWS secrets were found in the environment. >&2
+    echo >&2
+    echo Fix option 1: >&2
+    echo cp aws-secrets.include.dist aws-secrets.include >&2
+    echo and update the file with your credentials. >&2
+    echo >&2
+    echo Fix option 2:>&2
+    echo Manually set the environment variables AWS_ACCESS_KEY_ID and >&2
+    echo AWS_SECRET_ACCESS_KEY. >&2
+    echo >&2
+    echo Then run this script again. >&2
+    exit 1
+  fi
 }
 
 # check (and possibly install) dependencies
@@ -45,32 +69,17 @@ if [ $ansible_missing -eq 1 ] || [ $boto_missing -eq 1 ]; then
   install_deps
 fi
 
-# check AWS credentials
-test -f aws-secrets.include && . aws-secrets.include
-if [ -z "$AWS_ACCESS_KEY_ID" ] || [ -z "$AWS_SECRET_ACCESS_KEY" ]; then
-  echo Error: No AWS secrets were found in the environment. >&2
-  echo >&2
-  echo Fix option 1: >&2
-  echo cp aws-secrets.include.dist aws-secrets.include >&2
-  echo and update the file with your credentials. >&2
-  echo >&2
-  echo Fix option 2:>&2
-  echo Manually set the environment variables AWS_ACCESS_KEY_ID and >&2
-  echo AWS_SECRET_ACCESS_KEY. >&2
-  echo >&2
-  echo Then run this script again. >&2
-  exit 1
-fi
-
-ANSIBLE_PLAYBOOK="ansible-playbook -i $SCRIPT_DIR/ansible/ec2.py"
-
 action=$1
 case $action in
   initial-setup)
-    $ANSIBLE_PLAYBOOK ansible/aws-setup.yml
+    ensure_aws_creds
+    ansible-playbook -i $SCRIPT_DIR/ansible/ec2.py ansible/aws-setup.yml
     ;;
   staging)
-    $ANSIBLE_PLAYBOOK ansible/staging-aws.yml
+    ansible-playbook -i $SCRIPT_DIR/aws-static-inventory ansible/staging-aws.yml
+    ;;
+  staging-initdata)
+    ansible-playbook -i $SCRIPT_DIR/aws-static-inventory ansible/staging-aws-initdata.yml
     ;;
   "")
     echo ERROR: No action given. >&2
--- planet/generate.sh
+++ planet/generate.sh
@@ -1,7 +1,3 @@
 #/bin/sh
 
-TOPDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-cd $TOPDIR
 python generator/planet.py config.ini
-
