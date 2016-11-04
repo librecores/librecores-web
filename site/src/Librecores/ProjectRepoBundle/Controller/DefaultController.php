58100a61e80db6d289efdbc139da35b33d26b7ef
Elias Kouskoumvekakis <eliaskousk@gmail.com>
2016-11-04 12:13:46 +0200

    Add controller that removes the trailing slash
    So that URLs with a trailing slash do not produce errors.

--- site/src/Librecores/ProjectRepoBundle/Controller/DefaultController.php
+++ site/src/Librecores/ProjectRepoBundle/Controller/DefaultController.php
@@ -177,4 +177,15 @@ class DefaultController extends Controller
                 'users' => $users,
             ]);
     }
+
+
+    public function removeTrailingSlashAction(Request $request)
+    {
+        $pathInfo = $request->getPathInfo();
+        $requestUri = $request->getRequestUri();
+
+        $url = str_replace($pathInfo, rtrim($pathInfo, ' /'), $requestUri);
+
+        return $this->redirect($url, 301);
+    }
 }
