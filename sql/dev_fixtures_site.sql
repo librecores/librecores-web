dc9e5111350052fa8b5ea4078d88ffc221dbbc04
Philipp Wagner <mail@philipp-wagner.com>
2017-01-30 22:14:00 +0100

    Fix development "fixtures"

--- sql/dev_fixtures_site.sql
+++ sql/dev_fixtures_site.sql
@@ -1,13 +1,13 @@
 -- sample data for development of the LibreCores web site
 
 INSERT INTO `Organization` (`id`, `name`, `displayName`, `description`, `creatorId`, `createdAt`, `updatedAt`) VALUES
-(1, 'unassigned', 'Unassigned', 'Unassigned description', NULL, 2017-01-01 00:00:00, 2017-01-01 00:00:00),
-(2, 'openrisc', 'OpenRISC', 'OpenRISC Description', NULL, 2017-01-01 00:00:00, 2017-01-01 00:00:00);
+(1, 'unassigned', 'Unassigned', 'Unassigned description', NULL, NOW(), NOW()),
+(2, 'openrisc', 'OpenRISC', 'OpenRISC Description', NULL, NOW(), NOW());
 
 INSERT INTO `SourceRepo` (`id`, `type`, `url`, `stats_id`) VALUES
 (1, 'git', 'https://github.com/openrisc/mor1kx.git', NULL),
-(2, 'svn', 'http://opencores.org/ocsvn/openmsp430/openmsp430/trunk/', NULL);
+(2, 'git', 'https://github.com/optimsoc/sources', NULL);
 
 INSERT INTO `Project` (`id`, `name`, `parentOrganization_id`, `parentUser_id`, `projectUrl`, `issueTracker`, `sourceRepo_id`, `status`, `descriptionTextAutoUpdate`, `licenseName`, `licenseTextAutoUpdate`, `inProcessing`, `dateAdded`, `dateLastModified`) VALUES
 (1, 'mor1kx', 2, NULL, 'https://github.com/openrisc/mor1kx', 'https://github.com/openrisc/mor1kx/issues', 1, 'ASSIGNED', 1, 'OHDL', 1, 0, NOW(), NOW()),
-(2, 'openmsp430', 1, NULL, 'http://opencores.org/project,openmsp430', 'http://opencores.org/project,openmsp430,bugtracker', 2, 'UNASSIGNED', 1, 'LGPL', 1, 0, NOW(), NOW());
+(2, 'optimsoc', 1, NULL, 'http://www.optimsoc.org', 'https://github.com/optimsoc/issues', 2, 'UNASSIGNED', 1, 'MIT', 1, 0, NOW(), NOW());
