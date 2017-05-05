9b63c75ef6d43c18bf821c3a4f2de3932966d508
Philipp Wagner <mail@philipp-wagner.com>
2017-05-06 01:57:19 +0200

    Add QueueDispatcherService
    This is a convenience class for calling the RabbitMQ producer services
    directly, making the code easier to understand and possibly the backends
    (i.e. RabbitMQ) easier to exchange.

---
+++ site/src/Librecores/ProjectRepoBundle/Util/QueueDispatcherService.php
@@ -0,0 +1,31 @@
+<?php
+namespace Librecores\ProjectRepoBundle\Util;
+
+use Librecores\ProjectRepoBundle\Entity\Project;
+use Symfony\Component\DependencyInjection\ContainerAwareTrait;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+
+/**
+ * Dispatch requests to a asynchronous worker queue
+ *
+ * This class contains convenience wrappers for publishing requests to the
+ * queues used in LibreCores for asynchronous processing.
+ */
+class QueueDispatcherService implements ContainerAwareInterface
+{
+    use ContainerAwareTrait;
+
+    /**
+     * Update a project's information
+     *
+     * Calls the UpdateProjectInformation service for the given project.
+     *
+     * @param Project $project
+     */
+    public function updateProjectInfo(Project $project)
+    {
+        $this->container
+            ->get('old_sound_rabbit_mq.update_project_info_producer')
+            ->publish(serialize($project->getId()));
+    }
+}
