587489a7fb05a3b6926acd024214264b4e678751
Amitosh Swain Mahapatra <amitosh.swain@gmail.com>
2017-08-13 22:42:09 +0530

    Add doctrine migrations

--- site/app/DoctrineMigrations/Version20170813171042.php
+++ site/app/DoctrineMigrations/Version20170813171042.php
@@ -8,7 +8,7 @@ use Doctrine\DBAL\Schema\Schema;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-class Version20170809184905 extends AbstractMigration
+class Version20170813171042 extends AbstractMigration
 {
     /**
      * @param Schema $schema
@@ -18,7 +18,7 @@ class Version20170809184905 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE SourceRepo ADD forks INT DEFAULT NULL, ADD openPullRequests INT DEFAULT NULL, ADD openIssues INT DEFAULT NULL, ADD stars INT DEFAULT NULL, ADD watchers INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE Project ADD dateLastActivityOccurred DATETIME DEFAULT NULL, ADD forks INT DEFAULT NULL, ADD openPullRequests INT DEFAULT NULL, ADD openIssues INT DEFAULT NULL, ADD stars INT DEFAULT NULL, ADD watchers INT DEFAULT NULL');
     }
 
     /**
@@ -29,6 +29,6 @@ class Version20170809184905 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE SourceRepo DROP forks, DROP openPullRequests, DROP openIssues, DROP stars, DROP watchers');
+        $this->addSql('ALTER TABLE Project DROP dateLastActivityOccurred, DROP forks, DROP openPullRequests, DROP openIssues, DROP stars, DROP watchers');
     }
 }
