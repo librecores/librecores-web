06cfd06072965b9dd4c45985c1b801a5614f262b
Philipp Wagner <mail@philipp-wagner.com>
2019-01-09 11:06:58 +0000

    Remove deprecated ClassLoader
    Follow
    https://symfony.com/blog/new-in-symfony-3-3-deprecated-the-classloader-component
    to fix the following deprecation warning:
    
    Symfony\Component\HttpKernel\Kernel::doLoadClassCache() is deprecated since Symfony 3.3, to be removed in 4.0. {"exception":"[object] (ErrorException(code: 0): User Deprecated: Symfony\\Component\\HttpKernel\\Kernel::doLoadClassCache() is deprecated since Symfony 3.3, to be removed in 4.0. at /var/www/lc/site/vendor/symfony/symfony/src/Symfony/Component/HttpKernel/Kernel.php:461)"} []

--- site/web/app.php
+++ site/web/app.php
@@ -19,7 +19,6 @@ $apcLoader->register(true);
 //require_once __DIR__.'/../app/AppCache.php';
 
 $kernel = new AppKernel('prod', false);
-$kernel->loadClassCache();
 //$kernel = new AppCache($kernel);
 
 // When using the HttpCache, you need to call the method in your front controller instead of relying on the configuration parameter
