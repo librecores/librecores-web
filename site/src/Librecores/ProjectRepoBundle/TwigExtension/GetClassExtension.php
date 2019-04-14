36486b9a59b8dfd04434d5fd48e1296300363b15
Amitosh Swain Mahapatra <amitosh.swain@gmail.com>
2019-04-14 18:00:54 +0530

    Show an informative message if account is not yet activated

---
+++ site/src/Librecores/ProjectRepoBundle/TwigExtension/GetClassExtension.php
@@ -0,0 +1,22 @@
+<?php
+
+
+namespace Librecores\ProjectRepoBundle\TwigExtension;
+
+
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFunction;
+
+class GetClassExtension extends AbstractExtension
+{
+
+    function getFunctions() {
+
+        $filter = new TwigFunction('get_class', function($object) {
+            return get_class($object);
+        });
+
+        return [$filter];
+    }
+
+}
