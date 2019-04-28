6497b04cdeb7f656630fdb3f3000ed7fa8ae030b
Amitosh Swain Mahapatra <amitosh.swain@gmail.com>
2019-04-28 23:08:21 +0530

    Refresh project when crawling is complete
    For projects which are in processing state, poll backend every 10
    seconds for the status. Whenever the backend reports the crawl is
    complete, refresh the page.

---
+++ site/web/assets/js/project-auto-refresh.js
@@ -0,0 +1,16 @@
+$(function () {
+  let wasInProcessing = false;
+
+  function checkForUpdate() {
+    $.getJSON(window.location + '/crawl_status', function (status) {
+      if (status.inProcessing) {
+        wasInProcessing = true;
+        setTimeout(checkForUpdate, 10000);
+      } else if (wasInProcessing) {
+        window.location.reload();
+      }
+    });
+  }
+
+  checkForUpdate();
+});
