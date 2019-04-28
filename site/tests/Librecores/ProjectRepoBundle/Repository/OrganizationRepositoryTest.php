d6d38ce2270872b37c5d4690b4856a3c9ff5bdd8
Amitosh Swain Mahapatra <amitosh.swain@gmail.com>
2019-04-28 18:21:24 +0530

    Add tests for repositories to assert proper mapping with entities
    Prevents repositories being wrongly registered with an unintended
    entity

---
+++ site/tests/Librecores/ProjectRepoBundle/Repository/OrganizationRepositoryTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Librecores\ProjectRepoBundle\Repository;
+
+
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Mapping\ClassMetadata;
+use Librecores\ProjectRepoBundle\Entity\Organization;
+use PHPUnit\Framework\TestCase;
+use Symfony\Bridge\Doctrine\RegistryInterface;
+
+class OrganizationRepositoryTest extends TestCase
+{
+    public function testOrganizationRepositoryIsMappedToOrganizationEntity()
+    {
+        $mockEntityManager = $this->createMock(EntityManagerInterface::class);
+        $mockEntityManager->expects($this->once())
+            ->method('getClassMetadata')
+            ->with(Organization::class)
+            ->willReturn(new ClassMetadata(Organization::class));
+
+        $mockRegistry = $this->createMock(RegistryInterface::class);
+        $mockRegistry->expects($this->once())
+            ->method('getManagerForClass')
+            ->with(Organization::class)
+            ->willReturn($mockEntityManager);
+
+        /** @var RegistryInterface $mockRegistry */
+        $repository = new OrganizationRepository($mockRegistry);
+
+        $this->assertEquals(Organization::class, $repository->getClassName());
+    }
+}
