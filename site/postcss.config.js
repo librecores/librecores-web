04edf9d1f9a1a550f0a43dd4aa833763a06c1dc2
Philipp Wagner <mail@philipp-wagner.com>
2019-06-11 13:14:15 +0100

    Add two missing files
    These files were missing from the webpack commit by accident.

---
+++ site/postcss.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    plugins: {
+        // include whatever plugins you want
+        // but make sure you install these via yarn or npm!
+
+        // browserlist is in package.json
+        autoprefixer: {}
+    }
+}
