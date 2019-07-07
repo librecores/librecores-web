bde2eb496b4594ad6597bf4b5bb223c202bd420d
Aquib Baig <aquibbaig97@gmail.com>
2019-07-07 11:47:13 +0530

    Implement Web Notification Consumer

--- site/src/Consumer/WebNotificationConsumer.php
+++ site/src/Consumer/WebNotificationConsumer.php
@@ -2,8 +2,10 @@
 
 namespace App\Consumer;
 
-use App\Util\Notification;
+use App\Entity\Notification;
+use Doctrine\ORM\OptimisticLockException;
 use Psr\Log\LoggerInterface;
+use Mgilet\NotificationBundle\Manager\NotificationManager;
 
 /**
  * Class WebNotificationConsumer
@@ -15,25 +17,71 @@ use Psr\Log\LoggerInterface;
  */
 class WebNotificationConsumer extends AbstractNotificationConsumer
 {
+
+    /**
+     * @var NotificationManager
+     */
+    protected $notificationManager;
+
     /**
      * WebNotificationConsumer constructor
      *
-     * @param LoggerInterface $logger
+     * @param LoggerInterface     $logger
+     * @param NotificationManager $notificationManager
      */
-    public function __construct(LoggerInterface $logger)
-    {
+    public function __construct(
+        LoggerInterface $logger,
+        NotificationManager $notificationManager
+    ) {
         parent::__construct($logger);
+        $this->notificationManager = $notificationManager;
+    }
+
+    /**
+     * Should a notification be handled by this Consumer?
+     *
+     * @return bool
+     */
+    protected function shouldHandle(): bool
+    {
+        $type = $this->notification->getType();
+        switch ($type) {
+            case "new_project":
+                $this->logger->info('Processing the Notification...');
+
+                return true;
+            default:
+                $this->logger->info('Something went wrong. Check the Notification type');
+
+                return false;
+        }
     }
 
     /**
      * Handles Notifications that will be sent to UI
      *
      * @return bool
+     *
+     * @throws OptimisticLockException
      */
     protected function handle(): bool
     {
-        // TODO: Process the Notification($this->notification)
-        echo "Web Notification Consumer";
+        echo "\nWeb Notification Consumer\n";
+
+        $notification = new Notification();
+
+        // Populate the Notification Entity
+        $notification->setMessage($this->notification->getMessage());
+        $notification->setType($this->notification->getType());
+        $notification->setSubject($this->notification->getSubject());
+        $notification->setDate($this->notification->getCreatedAt());
+
+        // Persist the Notification
+        $this->notificationManager->addNotification(
+            [$this->notification->getRecipient()],
+            $notification,
+            true
+        );
 
         return true;
     }
