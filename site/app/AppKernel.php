ff0449bf9414489333690b0ca1fa7f1d9633bac4
Philipp Wagner <mail@philipp-wagner.com>
2020-05-07 12:07:03 +0100

    Add back getCacheDir()/getLogDir()
    These functions are needed in Symfony 3.4 to get their value in line
    with Symfony 4+. They can be removed once we switch to that version of
    Symfony.

--- site/app/AppKernel.php
+++ site/app/AppKernel.php
@@ -49,4 +49,14 @@ class AppKernel extends Kernel
     {
         $loader->load(__DIR__.'/config/config_'.$this->getEnvironment().'.yml');
     }
+
+    public function getCacheDir()
+    {
+        return dirname(__DIR__).'/var/cache/'.$this->environment;
+    }
+
+    public function getLogDir()
+    {
+        return dirname(__DIR__).'/var/logs';
+    }
 }
